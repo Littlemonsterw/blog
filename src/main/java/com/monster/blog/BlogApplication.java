eb121360964b1f3a2f4f5895388a0c14acc52a74
Littlemonsterw <1067533996@qq.com>
2019-12-23 21:56:22 +0800

    Swagger文档配置

--- src/main/java/com/monster/blog/BlogApplication.java
+++ src/main/java/com/monster/blog/BlogApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Monster
+ */
 @SpringBootApplication
-@MapperScan("")
+@MapperScan("com.monster.blog.mapper")
 public class BlogApplication {
 
     public static void main(String[] args) {

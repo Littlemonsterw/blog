e53683a5beb463270fefc3c4b78a20d919645591
Littlemonsterw <1067533996@qq.com>
2019-12-25 00:22:07 +0800

    JWT登录授权过滤器

---
+++ src/main/java/com/monster/blog/component/RestfulAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.monster.blog.component;
+
+import cn.hutool.json.JSONUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 当访问接口没有权限时，自定义的返回结果
+ * @author Monster
+ * @date 2019/12/24.
+ */
+@Component
+public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest httpServletRequest,
+                       HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.getWriter().println(JSONUtil.parse(e.getMessage()));
+        httpServletResponse.getWriter().flush();
+    }
+}

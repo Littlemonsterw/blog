e7cb212e49d9fe89e7083b264a03aa227b264167
wuhan <wuhan@lecent.cn>
2020-01-07 16:01:46 +0800

    添加获取用户所有权限的SQL

--- src/main/java/com/monster/blog/mapper/UserRoleMapper.java
+++ src/main/java/com/monster/blog/mapper/UserRoleMapper.java
@@ -1,11 +1,22 @@
 package com.monster.blog.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.monster.blog.entity.Permission;
 import com.monster.blog.entity.UserRole;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @author wuhan
  * @date 2019/12/31 15:41
  */
 public interface UserRoleMapper extends BaseMapper<UserRole> {
+
+    /**
+     * 获取用户权限
+     * @param userId
+     * @return
+     */
+    List<Permission> getPermissionList(@Param("userId") Long userId);
 }

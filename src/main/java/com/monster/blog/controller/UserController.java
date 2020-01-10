db625a65ea75b4436025ad4ef14cfcbc168adab1
wuhan <wuhan@lecent.cn>
2020-01-10 17:43:35 +0800

    添加分页插件PageHelper

--- src/main/java/com/monster/blog/controller/UserController.java
+++ src/main/java/com/monster/blog/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.monster.blog.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.monster.blog.common.api.R;
 import com.monster.blog.entity.User;
 import com.monster.blog.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiOperationSupport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author wuhan
@@ -19,12 +26,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "用户基本信息", tags = "用户基本信息接口")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
     @GetMapping("/detail")
     @ApiOperationSupport(order = 1)
     @ApiOperation(value = "详情", notes = "用户详情,传入User")
     public R<User> detail(String username) {
-        return null;
+        User user = userService.getOne(Wrappers.<User>lambdaQuery().eq(User::getUsername, username));
+        return R.data(user);
+    }
+
+    @GetMapping("/userInfoPage")
+    @ApiOperationSupport(order = 2)
+    @ApiOperation(value = "分页查询用户信息列表", notes = "用户信息列表（分页）")
+    public List<User> userInfoPage() {
+        PageHelper.startPage(1,5);
+        List<User> list = userService.list(Wrappers.query(new User()));
+        return list;
     }
 }

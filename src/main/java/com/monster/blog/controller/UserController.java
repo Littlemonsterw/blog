86f317db7fa458f6026d2c74e8e986134b2b7a71
wuhan <wuhan@lecent.cn>
2020-01-13 10:46:01 +0800

    用户分页查询优化

--- src/main/java/com/monster/blog/controller/UserController.java
+++ src/main/java/com/monster/blog/controller/UserController.java
@@ -1,20 +1,17 @@
 package com.monster.blog.controller;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.monster.blog.common.api.R;
 import com.monster.blog.entity.User;
 import com.monster.blog.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiOperationSupport;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -26,7 +23,7 @@ import java.util.List;
 @Api(value = "用户基本信息", tags = "用户基本信息接口")
 public class UserController {
 
-    @Autowired
+    @Resource
     private UserService userService;
 
     @GetMapping("/detail")
@@ -40,9 +37,8 @@ public class UserController {
     @GetMapping("/userInfoPage")
     @ApiOperationSupport(order = 2)
     @ApiOperation(value = "分页查询用户信息列表", notes = "用户信息列表（分页）")
-    public List<User> userInfoPage() {
-        PageHelper.startPage(1,5);
-        List<User> list = userService.list(Wrappers.query(new User()));
-        return list;
+    public List<User> userInfoPage(@RequestParam(defaultValue = "1") Integer pageNum, @RequestParam(defaultValue = "5") Integer pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        return userService.list();
     }
 }
